924066a193353f063b600923196e7c1c35c41e7c
joacasallas <joacasallas@gmail.com>
2022-06-03 12:58:16 -0500

    task 2. [1-hbnb.py, templates/1-hbnb.html, static/scripts/1-hbnb.js]

---
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -0,0 +1,24 @@
+/*
+SINTAXIS EQUIVALENTES FUNCTION READY()
+$( handler )
+$( document ).ready( handler )
+$( "document" ).ready( handler )
+$( "img" ).ready( handler )
+$().ready( handler )*/
+
+
+/*
+PROTOTIPO FUNCION READY()
+$( document ).ready( function() {
+    // Mi código de inicialización
+    $( window ).on("load", function(){
+       // Mi código de ejecucion
+    });
+} )*/
+
+$( document ).ready(function() {
+    let boxChecked = {};
+    $( document ).on('load', function(){
+        //code
+    });
+});
